6210ab5b50afc35372dfe59722e944f55734d7e8
jharry444 <jharrison1@qa.com>
2024-06-24 15:00:28 +0100

    Added update and delete

--- src/main/java/com/qa/lbg/rest/CatController.java
+++ src/main/java/com/qa/lbg/rest/CatController.java
@@ -1,7 +1,6 @@
 package com.qa.lbg.rest;
 
 import com.qa.lbg.domain.Cat;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,17 +10,12 @@ import java.util.List;
 @RestController
 public class CatController {
 
-
     private List<Cat> cats = new ArrayList<>();
 
     // this method will be called when a GET request is sent to /hello
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
-    }
-
-    int sum(int a, int b) {
-        return a + b;
     }
 
     @PostMapping("/cat/new")
@@ -38,5 +32,31 @@ public class CatController {
     @GetMapping("/cat/{id}")
     public Cat getCat(@PathVariable Integer id) {
         return this.cats.get(id);
+    }
+
+
+    @PatchMapping("/cat/{id}")
+    public Cat updateCat(@PathVariable(name = "id") Integer id,
+                         @RequestParam(name = "name", required = false) String name,
+                         @RequestParam(name = "colour", required = false) String colour,
+                         @RequestParam(name = "age", required = false) Integer age,
+                         @RequestParam(name = "nature", required = false) String nature,
+                         @RequestParam(name = "lives", required = false) Integer lives) {
+
+        Cat toUpdate = this.cats.get(id);
+
+        if (name != null) toUpdate.setName(name);
+        if (colour != null) toUpdate.setColour(colour);
+        if (age != null) toUpdate.setAge(age);
+        if (nature != null) toUpdate.setNature(nature);
+        if (lives != null) toUpdate.setLives(lives);
+
+        return toUpdate;
+    }
+
+    @DeleteMapping("/cat/{id}")
+    public Cat removeCat(@PathVariable Integer id) {
+        Cat toRemove = this.cats.get(id);
+        return this.cats.remove(id.intValue());
     }
 }
